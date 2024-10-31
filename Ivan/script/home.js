6d336f00de88135609c50c7a9f6ad75de6cd5d2e
Kiwi <ivajimalo@alu.edu.gva.es>
2024-10-31 16:53:35 +0100

    HTML, CSS y JS
    Código Front-End actualizado (Hay que acabarlo)

---
+++ Ivan/script/home.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.getElementById("alumn-button").addEventListener("click", function() {
+        window.location.href = "alumnos.html";
+    });
+
+    document.getElementById("client-button").addEventListener("click", function() {
+        window.location.href = "clientes.html";
+    });
+    
+    document.getElementById("relation-button").addEventListener("click", function() {
+        window.location.href = "relaciones.html";
+    });
+
+    document.getElementById("information-button").addEventListener("click", function() {
+        window.location.href = "informacion.html";
+    });
+      
+})
---
+++ Ivan/script/login.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Creaciones de oyente para llamar a funciones
+    let userButton = document.getElementById('siguiente')
+    userButton.addEventListener('click', nextUser)
+
+    let passwordButton = document.getElementById('iniciar-sesion')
+    passwordButton.addEventListener('click', nextPassword)
+
+    document.getElementById("home-button").addEventListener("click", function() {
+        window.location.href = "home.html";
+    });
+      
+})
+
+//Función para "cambiar de pantalla" al presionar un boton, escondemos el container de usuario y enseñamos para que introduzca contraseña
+function nextUser() {
+    document.getElementById('user-container').classList.add('hidden')
+    document.getElementById('password-container').classList.remove('hidden')
+}
+
+//Función para "cambiar de pantalla" al presionar un boton, escondemos el container de introducción de contraseña y mostramos un mensaje de bienvenida
+function nextPassword() {
+    const realUser = 'Vicente'
+    const realPassword = '1234'
+    let userName = document.getElementById('user').value
+    let userPwd = document.getElementById('password').value
+
+    if (realUser === userName && realPassword === userPwd) {
+        document.getElementById('password-container').classList.add('hidden')
+        document.getElementById('welcome').classList.remove('hidden')
+        document.getElementById('texto-bienvenida').innerText += '¡Bienvenido, '+ userName + '!'
+        userName = document.getElementById('user').value = ''
+        userPwd = document.getElementById('password').value = ''
+    } else {
+        alert('Prueba con usuario Vicente y contraseña 1234')
+        userName = document.getElementById('user').value = ''
+        userPwd = document.getElementById('password').value = ''
+        document.getElementById('password-container').classList.add('hidden')
+        document.getElementById('user-container').classList.remove('hidden')
+    }
+}
